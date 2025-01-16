8d7e338f0be7b456523f2f9b551fc0e1918e630f
jahred <jliddie@g.harvard.edu>
2025-01-15 19:27:34 -0500

    code correction

--- 2025/2025 - Week 2/tidytuesday_01142025.R
+++ 2025/2025 - Week 2/tidytuesday_01142025.R
@@ -13,8 +13,6 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 posit <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-01-14/conf2024.csv')
 
-wordcloud2(data = posit$session_title, size=1.6)
-
 count_posit <- posit %>%
   unnest_tokens(output = word, input = talk_title) %>%
   count(word, sort = TRUE)
